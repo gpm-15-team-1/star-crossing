ff23d034feb7dfa7d45abbdc57a78ce91127d0d7
Amanda Ohsiek <aohsiek@ucsc.edu>
2014-11-15 18:01:41 -0800

    Added Prefabs folder and a Note prefab w/ script
    The scene add/delete was kind of an accident, sorry

---
+++ Star Crossing/Assets/Scripts/Note.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Note : MonoBehaviour {
+
+	public string noteName;
+	public double position;
+	public double duration;
+	private bool hit;
+
+	// Use this for initialization
+	void Start () {
+		noteName = "c4";
+		position = 1;
+		duration = 1;
+		hit = false;
+	}
+
+	void HitNote () {
+		hit = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (hit) 
+		{
+			// draw note as explosion or whatever
+			// handle sound playback
+			// handle score
+			// set countdown for Destroy
+		}
+		else 
+		{
+			// draw note as a note
+			//this.transform = whatever;
+		}
+	}
+}
