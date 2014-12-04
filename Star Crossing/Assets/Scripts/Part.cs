eeb7cfc1cad9400965a78ba67492d74e6ff85f37
Amanda Ohsiek <aohsiek@ucsc.edu>
2014-12-04 00:13:37 -0800

    Notes!
    I have a dummy part with notes written into it. They scroll across the
    screen. Not sure what to do about the one note stuck in the middle of
    the screen though :/

--- Star Crossing/Assets/Scripts/Part.cs
+++ Star Crossing/Assets/Scripts/Part.cs
@@ -5,17 +5,40 @@ using System.Collections.Generic;
 public class Part : MonoBehaviour {
 
 	public string name;
+	public Note sampleNote;
 	public ArrayList notes;
 	//public List<Note> notes;
 	private bool playerControlled;
 
 	// Use this for initialization
 	void Start () {
-	
+		name = "melody";
+		notes = new ArrayList ();
+		playerControlled = true;
+		WritePart ();
 	}
-	
+
+	// Temp function that hard-codes in a dummy part
+	private void WritePart () {
+		notes.Add (Instantiate (sampleNote, new Vector3 (0, 0, 0), Quaternion.identity));
+		notes.Add (Instantiate (sampleNote, new Vector3 (2, 1, 0), Quaternion.identity));
+		notes.Add (Instantiate (sampleNote, new Vector3 (4, 2, 0), Quaternion.identity));
+		notes.Add (Instantiate (sampleNote, new Vector3 (6, 3, 0), Quaternion.identity));
+		notes.Add (Instantiate (sampleNote, new Vector3 (8, 4, 0), Quaternion.identity));
+		notes.Add (Instantiate (sampleNote, new Vector3 (10, 2, 0), Quaternion.identity));
+		notes.Add (Instantiate (sampleNote, new Vector3 (12, 0, 0), Quaternion.identity));
+		foreach (Note note in notes) {
+			note.transform.parent = gameObject.transform;
+		}
+	}
+
+	// FixedUpdate is a better Update
+	void FixedUpdate () {
+		this.transform.position = new Vector3 (this.transform.position.x - 3 * Time.deltaTime, this.transform.position.y, this.transform.position.z);
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
