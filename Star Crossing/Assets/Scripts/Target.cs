06f3582593330143c51403ca92055c857cde0660
Amanda Ohsiek <aohsiek@ucsc.edu>
2014-12-06 19:08:58 -0800

    Visual improvements to rhythm mode!
    SongScene now has a background, sprite notes instead of sphere notes,
    and a cursor/target that slides up and down on a track.

---
+++ Star Crossing/Assets/Scripts/Target.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+	public float mouseTargetScalar;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float yPos = (Input.mousePosition.y - Screen.height / 2) * mouseTargetScalar;
+		this.transform.position = new Vector3 (this.transform.position.x, yPos, this.transform.position.z);
+	}
+}
