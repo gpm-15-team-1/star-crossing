1b98cb0315630495b279d668f6617a77217ea35f
Amanda Ohsiek <aohsiek@ucsc.edu>
2015-01-14 00:35:46 -0800

    Update 1/14
    Notes disappear when clicked now! Running accuracy display is a work in
    progress.

--- Star Crossing/Assets/Scripts/Note.cs
+++ Star Crossing/Assets/Scripts/Note.cs
@@ -8,6 +8,8 @@ public class Note : MonoBehaviour {
 	public double duration;
 	public bool sustain;
 	public int pointValue;
+	private bool mouseClicked;
+	private bool isActive;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +18,9 @@ public class Note : MonoBehaviour {
 		duration = 1;
 		sustain = false;
 		pointValue = 5;
-		animation.Play ();
+		mouseClicked = false;
+		isActive = true;
+		//animation.Play ();
 	}
 
 //	void HitNote () {
@@ -25,7 +29,34 @@ public class Note : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-//		if (hit) 
+		if (Input.GetMouseButtonDown (0)) {
+			mouseClicked = true;
+		}
+		else {
+			mouseClicked = false;
+		}
+	}
+	
+	// Alternate update that happens after normal update
+	void LateUpdate () {
+		GameObject target = GameObject.Find ("Target");
+		GameObject songManager = GameObject.Find ("SongManager");
+		if (mouseClicked && (target.transform.position.x < this.transform.position.x + .5) && 
+		    (target.transform.position.x > this.transform.position.x - .5) &&
+		    (target.transform.position.y < this.transform.position.y + .5) &&
+		    (target.transform.position.y > this.transform.position.y - .5)) {
+			songManager.GetComponent<SongManager>().PossiblePoints += 5;
+			songManager.GetComponent<SongManager>().AcquiredPoints += 5;
+			Destroy(this.gameObject);
+		}
+		else {
+			if (this.transform.position.x < -4 && isActive) {
+				songManager.GetComponent<SongManager>().PossiblePoints += 5;
+				isActive = false;
+				//Destroy(this.gameObject);
+			}
+		}
+//		if (note was hit just now) 
 //		{
 //			// draw note as explosion or whatever
 //			// handle sound playback
@@ -35,7 +66,7 @@ public class Note : MonoBehaviour {
 //		else 
 //		{
 //			// draw note as a note
-//			//this.transform = whatever;
+//			// if note has passed the target's track (i.e. the player missed it), do score things
 //		}
 	}
 }
---
+++ Star Crossing/Assets/Scripts/SongManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SongManager : MonoBehaviour {
+
+	public Song sampleSong;
+	public Canvas textCanvas;
+	int possiblePoints;
+	int acquiredPoints;
+	Object theSong;
+
+	public int PossiblePoints
+	{
+		get { return possiblePoints; }
+		set { possiblePoints = value; }
+	}
+
+	public int AcquiredPoints
+	{
+		get { return acquiredPoints; }
+		set { acquiredPoints = value; }
+	}
+
+	// Use this for initialization
+	void Start () {
+		theSong = Instantiate (sampleSong);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		double accuracy = (double)acquiredPoints / (double)possiblePoints;
+	}
+}
