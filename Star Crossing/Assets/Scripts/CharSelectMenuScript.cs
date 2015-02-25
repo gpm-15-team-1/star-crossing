6212a45d0e7349134a90110a05da7634ffc6203b
Amanda Ohsiek <aohsiek@ucsc.edu>
2015-02-24 20:32:12 -0800

    Update 2/24
    Reformatted the structure of the "Feedback" file. Now the first
    character listed is the one being played (mood is included), and the
    ones after it are the NPCs (accuracy is included). Cleaned up the code
    for that scene while I was at it.

--- Star Crossing/Assets/Scripts/CharSelectMenuScript.cs
+++ Star Crossing/Assets/Scripts/CharSelectMenuScript.cs
@@ -5,38 +5,32 @@ using System.IO;
 
 public class CharSelectMenuScript : MonoBehaviour {
 
-	private GameObject[] highlights;
-	private GameObject[] accuracies;
-	//private string selectedChar;
+	public GameObject[] highlights;
+	public GameObject[] accuracies;
+	private int selectedChar;
 
 	void Start () {
-		// Initialize references.
-		//selectedChar = "";
-		highlights = GameObject.FindGameObjectsWithTag ("MenuHighlights");
+		selectedChar = -1;
 		foreach (GameObject highlight in highlights) {
 			highlight.GetComponent<Image>().color = Color.clear;
 		}
-		accuracies = GameObject.FindGameObjectsWithTag ("MenuAccuracies");
 		GameObject.Find ("GoButton").GetComponent<Button> ().interactable = false;
 	}
 
-	public void CharacterOnClick(string charName) {
-		// Set the clicked character as selected!
-		//selectedChar = charName;
-		string keyLetter = charName.Substring (0, 1);
+	public void CharacterOnClick(int whichChar) {
+		selectedChar = whichChar;
 		// Set highlight of only the clicked character to visible.
-		foreach (GameObject highlight in highlights) {
-			if (highlight.name.EndsWith(keyLetter)) {
-				highlight.GetComponent<Image>().color = Color.white;
-			} else
-				highlight.GetComponent<Image>().color = Color.clear;
-		}
-		// Set accuracy of only the clicked character to null.
-		foreach (GameObject accuracy in accuracies) {
-			if (accuracy.name.EndsWith(keyLetter)) {
-				accuracy.GetComponent<CharMenuAccuracyScript>().ClearAccuracy();
-			} else
-				accuracy.GetComponent<CharMenuAccuracyScript>().CalculateAccuracy();
+		for (int i=0; i<5; i++) {
+			// If the current index matches whichChar, highlight and clear accuracy.
+			if (i == whichChar){
+				highlights[i].GetComponent<Image>().color = Color.white;
+				accuracies[i].GetComponent<CharMenuAccuracyScript>().ClearAccuracy();
+			}
+			// Otherwise, hide highlight and display accuracy normally.
+			else {
+				highlights[i].GetComponent<Image>().color = Color.clear;
+				accuracies[i].GetComponent<CharMenuAccuracyScript>().CalculateAccuracy();
+			}
 		}
 		// Make "Go!" clickable.
 		GameObject.Find ("GoButton").GetComponent<Button> ().interactable = true;
@@ -46,20 +40,24 @@ public class CharSelectMenuScript : MonoBehaviour {
 	public void GoOnClick() {
 		// Before we switch scenes, we must save some data.
 		StreamWriter file = new StreamWriter(Application.dataPath + "/Resources/Files/Feedback.txt", false);
-		foreach (GameObject accuracy in accuracies) {
-			string name = accuracy.GetComponent<CharMenuAccuracyScript>().character.name;
-			string acc = accuracy.GetComponent<Text>().text;
-			// If acc is blank, that character is being played. Provide mood.
-			if (acc == "--") {
-				string mood = accuracy.GetComponent<CharMenuAccuracyScript>().character.mood.ToString();
-				file.WriteLine("PLAY " + name + " " + mood);
+		// First pass: write line with the selected character and his/her mood
+		for (int i=0; i<5; i++) {
+			if (i == selectedChar) {
+				string name = accuracies [i].GetComponent<CharMenuAccuracyScript> ().character.name;
+				string mood = accuracies [i].GetComponent<CharMenuAccuracyScript> ().character.mood.ToString ();
+				file.WriteLine (name + " " + mood);
 			}
-			// If acc is a percent, that character is on autopilot. Provide accuracy.
-			else if (acc.EndsWith("%")) {
-				file.WriteLine("NPC " + name + " " + acc);
+		}
+		// Second pass: write other characters and their calculated accuracies
+		for (int i=0; i<5; i++) {
+			if (i != selectedChar) {
+				string name = accuracies [i].GetComponent<CharMenuAccuracyScript> ().character.name;
+				string acc = accuracies[i].GetComponent<Text>().text;
+				file.WriteLine (name + " " + acc.Substring(0, acc.Length-1));
 			}
 		}
 		file.Close ();
+		// Now we can switch scenes!
 		Application.LoadLevel ("Rhythm Mode Prototype");
 	}
 }
